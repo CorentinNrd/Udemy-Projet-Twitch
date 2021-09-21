e49e656ced1fc30edc2eadbaa9b510d760aa7f80
Corentin Nordmann <corentinnordmann@MacBook-Pro-de-Corentin-2.local>
2021-09-21 12:04:01 +0200

    [add] component pour les erreurs

---
+++ src/components/Erreur/Erreur.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Erreur() {
+
+    return (
+
+        <div className="containerDecaleResultats">
+
+            <h4>Résultats de recherche : pas de résultats, <br /> Vérifiez l'aurtographe de votre saisie, ou ce streameur n'existe pas.</h4>
+
+        </div>
+
+    )
+
+
+}
+
+export default Erreur;
